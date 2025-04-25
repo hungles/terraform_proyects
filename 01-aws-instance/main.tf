78d193c4e807ac0c6beea7ccb70cbdac445815e5
Sebastian Carmona <scarmona04@hotmail.com>
2025-04-24 23:33:34 -0500

    Create my first Terraform Proyect

---
+++ 01-aws-instance/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1" // replace with your desired region
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0b6a0fb2d405b768d" // replace with your subnet ID
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
